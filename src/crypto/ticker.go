e441ad126e76b33782706839cd7403f24f9333a7
Dmitry Z <zagvozkin.dmitry@gmail.com>
2021-01-20 18:07:48 +0600

    Ticker timestamp to time.Time, close #3

--- src/crypto/ticker.go
+++ src/crypto/ticker.go
@@ -1,9 +1,11 @@
 package crypto
 
-// Ticker interface is used to provide  information from different Exchanges
+import "time"
+
+// Ticker interface is used to provide information from different Exchanges
 type Ticker interface {
 	Pair() (Pair, error)
 	BestBid() (float64, error)
 	BestAsk() (float64, error)
-	Timestamp() int64
+	Timestamp() time.Time
 }
